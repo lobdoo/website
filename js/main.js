fa39ea921fc39aa2e97ace83656e55821e6a4770
Bold-Erdene <bolderdenez40@gmail.com>
2020-02-12 03:28:40 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function home() {
+    let elmnt1 = document.getElementById('1')
+    elmnt1.scrollIntoView();
+  }
+  function photos() {
+    let elmnt2 = document.getElementById('2')
+    elmnt2.scrollIntoView();
+  }
+  function project() {
+    let elmnt3 = document.getElementById('3')
+    elmnt3.scrollIntoView();
+  }
+  function hobby() {
+    let elmnt4 = document.getElementById('4')
+    elmnt4.scrollIntoView();
+  }
+  function contact() {
+    let elmnt4 = document.getElementById('5')
+    elmnt4.scrollIntoView();
+  }
+
+
+
